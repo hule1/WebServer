898c71de8669d8f0ad3fa3e6a7b74070a00fdc0e
hule1 <997971814@qq.com>
2023-07-27 16:29:10 +0800

    webserver

--- code/server/webserver.h
+++ code/server/webserver.h
@@ -2,13 +2,13 @@
  * @Author       : mark
  * @Date         : 2020-06-17
  * @copyleft Apache 2.0
- */ 
+ */
 #ifndef WEBSERVER_H
 #define WEBSERVER_H
 
 #include <unordered_map>
-#include <fcntl.h>       // fcntl()
-#include <unistd.h>      // close()
+#include <fcntl.h>  // fcntl()
+#include <unistd.h> // close()
 #include <assert.h>
 #include <errno.h>
 #include <sys/socket.h>
@@ -23,53 +23,53 @@
 #include "../pool/sqlconnRAII.h"
 #include "../http/httpconn.h"
 
-class WebServer {
+class WebServer
+{
 public:
     WebServer(
-        int port, int trigMode, int timeoutMS, bool OptLinger, 
-        int sqlPort, const char* sqlUser, const  char* sqlPwd, 
-        const char* dbName, int connPoolNum, int threadNum,
-        bool openLog, int logLevel, int logQueSize);
+        int port, int trigMode, int timeoutMS, bool OptLinger, // 端口，触发模式，延时时间，优雅的关闭
+        int sqlPort, const char *sqlUser, const char *sqlPwd,  // 数据库端口，用户，密码
+        const char *dbName, int connPoolNum, int threadNum,    // 数据库名字，连接池数量，线程数量
+        bool openLog, int logLevel, int logQueSize);           // 开启日志，日志等级，日志队列大小
 
-    ~WebServer();
-    void Start();
+    ~WebServer(); // 析构函数
+    void Start(); // 开始
 
 private:
-    bool InitSocket_(); 
-    void InitEventMode_(int trigMode);
-    void AddClient_(int fd, sockaddr_in addr);
-  
-    void DealListen_();
-    void DealWrite_(HttpConn* client);
-    void DealRead_(HttpConn* client);
+    bool InitSocket_();                        // 初始化Socket，用于TCP
+    void InitEventMode_(int trigMode);         // 初始化事件模式，边沿触发，ET
+    void AddClient_(int fd, sockaddr_in addr); // 添加客户端
 
-    void SendError_(int fd, const char*info);
-    void ExtentTime_(HttpConn* client);
-    void CloseConn_(HttpConn* client);
+    void DealListen_();                // 处理监听
+    void DealWrite_(HttpConn *client); // 处理写
+    void DealRead_(HttpConn *client);  // 处理读
 
-    void OnRead_(HttpConn* client);
-    void OnWrite_(HttpConn* client);
-    void OnProcess(HttpConn* client);
+    void SendError_(int fd, const char *info); // 发送错误
+    void ExtentTime_(HttpConn *client);        // 拓展时间
+    void CloseConn_(HttpConn *client);         // 关闭连接
 
-    static const int MAX_FD = 65536;
+    void OnRead_(HttpConn *client);   // 读
+    void OnWrite_(HttpConn *client);  // 写
+    void OnProcess(HttpConn *client); // 正在处理
 
-    static int SetFdNonblock(int fd);
+    static const int MAX_FD = 65536; // 最大文件描述符，类成员所有对象共享
+
+    static int SetFdNonblock(int fd); // 设置文件描述符非阻塞，类成员所有对象共享
 
     int port_;
     bool openLinger_;
-    int timeoutMS_;  /* 毫秒MS */
+    int timeoutMS_; /* 毫秒MS */
     bool isClose_;
     int listenFd_;
-    char* srcDir_;
-    
+    char *srcDir_;
+
     uint32_t listenEvent_;
     uint32_t connEvent_;
-   
+
     std::unique_ptr<HeapTimer> timer_;
     std::unique_ptr<ThreadPool> threadpool_;
     std::unique_ptr<Epoller> epoller_;
     std::unordered_map<int, HttpConn> users_;
 };
 
-
-#endif //WEBSERVER_H
+#endif // WEBSERVER_H
